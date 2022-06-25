110bb7409487229025ca43462d751aa715c1f415
Mohamed-codes <mohamediro98@gmail.com>
2022-06-26 01:10:31 +0200

    -m "added dating app"

--- helloGit.py
+++ helloGit.py
@@ -1,2 +1,12 @@
 udi = "statsborgerskap"
 print(udi)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
